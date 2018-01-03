3c46910ba6adbcc01fe4e96e69d9c3384bd23f7f
Dr. Levente L. Orbán <llorban@users.noreply.github.com>
2018-01-03 15:39:10 -0800

    Create 1 intro.r

---
+++ live/1 intro.r
@@ -0,0 +1,35 @@
+# After starting R, the console is ready for input. You can enter values next to the prompt (>), and press Enter to perform calculations. 
+> 1 + 2
+[1] 3
+
+# To square numbers, use the hat (^) sign:
+> 3^2
+[1] 9
+
+# Use slash (/) to divide numbers and star (*) to multiply them:
+> 4/4
+[1] 1
+
+> 7*7
+[1] 49
+
+# You can assign values to variables using the operator "=". Just typing the variable by itself at the prompt will print out the value. Another form of the operator is "<-" is also good.
+> x = 1
+> x
+[1] 1
+
+# An R function is invoked by its name, then followed by the parenthesis, and zero or more arguments. The following applies the function c to combine three numberic values into a vector.
+> y = c(1,2,3)
+[1] 1 2 3 
+
+# Vectors can contain string values too
+> colours = c("red","yellow","blue")
+
+# Text after the hastag sign within the samel line is considered a comment: 
+> 1 + 1   # this is a comment
+
+## Data Types
+# Numerics
+> k = 1
+> k
+k

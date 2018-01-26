26f0a07d9b607158a3808f0035dbdee24494b5e4
Dr. Levente L. Orbán <llorban@users.noreply.github.com>
2018-01-26 00:11:11 -0800

    Update and rename 1-generateDataFrames.r to 1-generateDataFrames.md

--- live_homework/1-generateDataFrames.r
+++
@@ -1,6 +0,0 @@
-# Create a data frame that contains three columns of data: a subject id, a nominal variable 
-# (contains a string or text value), and a numeric variable (interval or ratio scale). 
-# Create a micro-study with two questions that match the data structure and gather data from 5 or 
-# more family/friend/coworker/etc. 
-
-# Save and submit the R data file here. 

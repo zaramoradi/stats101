c0633a66fd9f023e93de4818ebf103b9025243b4
Dr. Levente L. Orbán <llorban@users.noreply.github.com>
2018-02-07 11:09:20 -0800

    Create exam1.r

---
+++ live_exams/exam1.r
@@ -0,0 +1,104 @@
+## EXAM 1 (210 points)
+## PSYC 2300 S11 2018-10 LLO
+
+#################################################################################
+#################################################################################
+## PREAMBLE
+## You can use your notes, textbook and web during the exam.
+## Communication either with a classmate, over the phone or over the web is absolutely forbidden. 
+## Keep your work concise and clean: use headings
+## Do not display variable contents or use redundant lines of code
+## All of your calculations must be done manually. 
+## Do not do unnecessary calculations or create variables not required to answer a question. 
+##   Save intermediate variable required for a final computation. 
+##   E.g., if the question asks for a mean, save the sum and length variables, but not save a frequency table
+## You are encouraged to double check your work but do not include any of those lines of code.
+## All of your answers must be supported by calculations for full points. 
+## Partial points can be achieved even with errors so do not panic.
+## Answer conceptual questions as a hashtag comment. For full marks include lecture discussions
+
+#################################################################################
+#################################################################################
+## DATASET 1 -- 65 points
+## Link: http://llorban.net/psyc2300/chimpcookingstudy.csv
+## 2015 Warneken and Rosati
+## Data Synposis: The transition to a cooked diet represents an important shift in human ecology and evolution. 
+## Cooking requires a set of sophisticated cognitive abilities, including causal reasoning, 
+## self-control and anticipatory planning. Do humans uniquely possess the cognitive capacities 
+## needed to cook food? We address whether one of humans' closest relatives, chimpanzees (Pan troglodytes),
+## possess the domain-general cognitive skills needed to cook. The percentage of cooked column in this data set
+## indicates the preference of chimpanzees for cooked food over raw food. Subject names refer to individual chimps.
+
+## Generate a frequency table (5 points)
+
+## What is the sample size? (5 points)
+
+## What is the sum of all scores? (5 points)
+
+## Generate a histogram of the "percentage of cooked" column (5 points)
+
+## What is the mean, median and mode of the choice_cooked_percent column?  (10 points)
+
+## What is the range of scores of the proportion correct, and the time at bin variables?  (10 points)
+
+## What is the shape of the choice_cooked_percent distribution?  (5 points)
+
+## Conceptual: Based on your reading of the descriptive statistics, do you think chimps prefer 
+## cooked foods? Explain. (5 points)
+
+## Conceptual: According to lecture, what is the relationship 
+## between mathematics and empiricism? Explain.  (15 points)
+
+#################################################################################
+#################################################################################
+## DATASET 2 -- 70 points
+## Link: "http://llorban.net/psyc2300/EI_score.csv"
+## 2014 Pan, Wang, Wang, Hitchman, Wang and Chen
+## Emotional intelligence (EI) is a multi-faceted construct consisting of our ability to perceive, 
+## monitor, regulate and use emotions. Despite much attention being paid to the neural substrates of 
+## EI, little is known of the spontaneous brain activity associated with EI during resting state. We 
+## used resting-state fMRI to investigate the association between the amplitude of low-frequency 
+## fluctuations (ALFFs) and EI in a large sample of young, healthy adults. 
+
+## Compute the standard deviation of the Total column  (5 points)
+
+## Standardize the "Total" scores (30 points)
+
+## Conceptual: Interpret the meaning of the score 0.05479849 in the context of the normal curve (5 points)
+
+## Conceptual: What is the highest z-score in this distribution? Where does this fall on the normal curve? (5 points)
+
+## Conceptual: What is the lowest z-score in this distribution? Where does this fall on the normal curve? (5 points)
+
+## Generate a boxplot of the Total scores as a function of age.  (5 points)
+
+## Conceptual: Are there age groups that appear to have markedly different EQ scores? (5 points)
+
+## Generate a boxplot of the Total scores as a function of gender (5 points)
+
+## Conceptual: Is there a difference in EQ scores across the sexes? (5 points)
+
+#################################################################################
+#################################################################################
+## DATASET 3 -- 80 points
+## Link: http://llorban.net/psyc2300/garbage_disposal.csv
+## 2016, Wu, DiGiacomo, Lenkic, Wong and Kingstone (UBC)
+## Synopsis: Previous observational work revealed that transient populations in a sustainable building disposed 
+## of waste more accurately when compared to patrons in a non-sustainable building. The current study 
+## uses an experimental design to replicate this observed effect and to investigate whether or not 
+## the built environment influences motivational factors to impact behavior. Garbage disposal behaviour
+## was compared in a sustainable and a conventional building. Condition "1" refers to a sustainable building
+## and condition "2" refers to a conventional building. Proportion correct refers to the number of garbage
+## disposals in the garbage bin that were correctly placed. Time at the bin refers to the amount of time
+## subjects spent sorting the garbage. 
+
+## Standardize the proportion correct and time at bin variables (50 points)
+
+## By judging the z-scores, which one of the two variables has the more extreme maximum, and 
+## more extreme minimum values? Explain. (20 points)
+
+## Generate a scatterplot of the proportion of correct and time at bin variables (5 points)
+
+## Conceptual: Is there a relationship between the two variables? Explain. (5 points)
+
+
